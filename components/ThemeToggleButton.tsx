5e83f939e8e3212d341ea37a211d315a9e52e9f7
angelzxz1 <angel.zuna@icloud.com>
2022-11-15 09:02:06 -0500

    Edited toggle button

--- components/ThemeToggleButton.tsx
+++ components/ThemeToggleButton.tsx
@@ -1,19 +1,34 @@
-import { IconButton, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import {
+    IconButton,
+    useColorMode,
+    useColorModeValue,
+    Box,
+} from "@chakra-ui/react";
 import { MdOutlineWbSunny as SunIcon } from "react-icons/md";
 import { BsMoonStarsFill as MoonIcon } from "react-icons/bs";
 // import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 const ThemeToggleButton = () => {
-	const { toggleColorMode } = useColorMode();
-	return (
-		<IconButton
-			aria-label="Toggle theme"
-			colorScheme={useColorModeValue("purple", "orange")}
-			icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
-			onClick={toggleColorMode}
-			borderRadius="full"
-			zIndex={10}
-		/>
-	);
+    const { toggleColorMode } = useColorMode();
+    return (
+        <Box
+            bg={useColorModeValue("black", "white")}
+            onClick={toggleColorMode}
+            borderRadius="full"
+            zIndex={10}
+            p={4}
+            boxShadow={`0 0 5px ${useColorModeValue("black", "white")} `}
+            cursor="pointer"
+            _hover={{
+                boxShadow: `0 0 10px ${useColorModeValue("black", "white")} `,
+            }}
+            transition="all 0.3s ease"
+        ></Box>
+        // <IconButton
+        //     aria-label="Toggle theme"
+        //
+        //
+        // />
+    );
 };
 
 export default ThemeToggleButton;
