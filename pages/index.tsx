f5a9d33da37fc21388f8e77886e5687f321b07a4
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2022-11-15 23:30:15 +0000

    Update 5 files

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,13 @@ const Inicio = () => {
   return (
     <Box>
       <Image
-        src={useColorModeValue("/MainPageLogoWhite.png", "/MainPageLogo.jpg")}
+        src={useColorModeValue(
+          "/MainPageLogo-NoBG-W.png",
+          "/MainPageLogo-NoBG.png"
+        )}
+        mt={1}
         alt="main-page-logo"
+        // filter={`drop-shadow(0 0 5px ${useColorModeValue("black", "white")})`}
       />
       <Grid>
         <Box
